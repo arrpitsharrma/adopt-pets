a495b674f6460d38893587aea50ab981e06c403a
arpitsharrma <arpit.sharma@sjsu.edu>
2020-05-27 13:27:10 -0700

    Implemented Vue State for getting data of particular cat/dog

---
+++ src/views/Pet.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>
+        Name: {{ animal.name }} ({{ $route.params.species }})
+    </h1>
+    <p>Breed: {{ animal.breed }}</p>
+    <p>Age: {{ animal.age }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  data () {
+    return {
+      animal: {}
+    }
+  },
+  computed: {
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  mounted () {
+    const animal = this[this.$route.params.species][this.$route.params.id]
+    this.animal = animal
+  }
+}
+</script>
